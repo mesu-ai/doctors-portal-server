bae2e7e5797539c9923e33b46eb0647bb5ff1aca
mesu-ai <mesu.s.ss@gmail.com>
2021-11-09 05:08:38 +0600

    date filter

--- index.js
+++ index.js
@@ -18,20 +18,35 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 async function run() {
     try {
-      await client.connect();
-      const database = client.db("doctors-portal");
-      const appointmentDB = database.collection("appointment");
-      
+        await client.connect();
+        const database = client.db('doctors_portal');
+        const appointmentsCollection = database.collection('appointments');
 
-      console.log('connect to db');
+        app.get('/appointments', async (req, res) => {
+            const email = req.query.email;
+            const date = new Date(req.query.date).toLocaleDateString();
 
+            const query = { email: email, date: date }
 
-    } finally {
-    //   await client.close();
+            const cursor = appointmentsCollection.find(query);
+            const appointments = await cursor.toArray();
+            res.json(appointments);
+        })
+
+        app.post('/appointments', async (req, res) => {
+            const appointment = req.body;
+            const result = await appointmentsCollection.insertOne(appointment);
+            console.log(result);
+            res.json(result)
+        });
+
     }
-  }
-  run().catch(console.dir);
+    finally {
+        // await client.close();
+    }
+}
 
+run().catch(console.dir);
 
 
 
